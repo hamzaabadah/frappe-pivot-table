de59e5efd0968d6875737d298b821cf44b4bb0cb
HAMZA ABUABADA <abadahhamza@gmail.com>
2023-04-08 00:09:00 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("requirements.txt") as f:
 	install_requires = f.read().strip().split("\n")
 
 # get version from __version__ variable in permission_level_for_reports/__init__.py
-from permission_level_for_reports import __version__ as version
+from pivot_table import __version__ as version
 
 setup(
 	name='pivot_table',
